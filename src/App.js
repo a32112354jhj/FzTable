ce290f612fc5b02339ad1f8c5090557c3e59270c
jiahao <a32112354jhj@gmail.com>
2019-05-17 00:33:10 +0800

    jsx update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './style.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <div className="fz_table_box">
+          <div className="left_box"></div>
+          <div className="right_box">
+            
+          </div>
+      </div>
     </div>
   );
 }
