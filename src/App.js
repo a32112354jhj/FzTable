5a995dbc8a21dccdb255fba94879cfe17e100dba
jiahao <a32112354jhj@gmail.com>
2019-05-21 14:58:57 +0800

    slider

--- src/App.js
+++ src/App.js
@@ -1,18 +1,419 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './style.css';
 
-function App() {
-  return (
-    <div className="App">
-      <div className="fz_table_box">
-          <div className="left_box"></div>
-          <div className="right_box">
-            
+export default class App extends Component {
+
+  show = 3; //顯示數量
+  slide = 1; //滑動格數
+  btn_show = ['btn_show', 'btn_hide'];
+  
+  state = {
+    scroll_position: 0,
+    box_size: 0,
+    scroll_style: {
+      transition: this.props.speed + 's',
+      left: 0 + '%'
+    }
+  }
+
+
+  constructor(props) {
+    super(props);
+
+    // 移動格數
+    this.slide = typeof (props.count.slide) !== 'undefined' ? props.count.slide : this.slide;
+    if (this.slide > 4) {
+      this.slide = 4;
+    }
+    else if (this.slide <= 0) {
+      this.slide = 1;
+    }
+
+    // M版一次顯示格數
+    this.show = typeof (props.count.show) !== 'undefined' ? props.count.show : this.show;
+    if (this.show > 4) {
+      this.show = 4;
+    }
+    else if (this.show <= 0) {
+      this.show = 1;
+    }
+  }
+
+  // 按鈕動作
+  slideLeft = () => {
+    let scroll_position = this.state.scroll_position - (100 / this.show) * this.slide;
+
+    if (this.show == 4 && scroll_position <= -75) {
+      scroll_position = -75;
+    }
+    else if (this.show == 3 && scroll_position <= -133.333) {
+      scroll_position = -133.333;
+    }
+    else if (this.show == 2 && scroll_position <= -250) {
+      scroll_position = -250;
+    }
+    else if (this.show == 1 && scroll_position <= -600) {
+      scroll_position = -600;
+    }
+
+    this.setState({
+      scroll_position: scroll_position,
+      scroll_style: {
+        transition: this.props.speed + 's',
+        left: scroll_position + '%',
+      }
+    });
+  }
+
+  slideRight = () => {
+    let scroll_position = this.state.scroll_position + (100 / this.show) * this.slide;
+
+    if (scroll_position >= 0) {
+      scroll_position = 0;
+    }
+
+    this.setState({
+      scroll_position: scroll_position,
+      scroll_style: {
+        transition: this.props.speed + 's',
+        left: scroll_position + '%',
+      }
+    });
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <div className={"fz_table_box cfx" + " mob_" + this.show}>
+          <div className="left_box">
+            <div className="table_title">
+              <span>回程</span>
+              <span>去程</span>
+            </div>
+            <div><span className="day">12/27(一)</span></div>
+            <div><span className="day">12/28(二)</span></div>
+            <div><span className="day">12/29(三)</span></div>
+            <div><span className="day">12/30(四)</span></div>
+            <div><span className="day">12/31(五)</span></div>
+            <div><span className="day"><i>2018</i>01/01(六)</span></div>
+            <div><span className="day">01/02(日)</span></div>
           </div>
+          {/* 右框 */}
+          <div className="right_box">
+            {/* 左按鈕 */}
+            <button type="button" className="slide_left slide_btn" onClick={this.slideLeft}>
+              <i className="fas fa-chevron-left"></i>
+            </button>
+            {/* 右按鈕 */}
+            <button type="button" className="slide_right slide_btn" onClick={this.slideRight}>
+              <i className="fas fa-chevron-right"></i>
+            </button>
+
+            <div className="scroll_box" style={this.state.scroll_style}>
+              {/* 日期 */}
+              <div className="top_title">
+                <div><span className="day">12/27(一)</span></div>
+                <div><span className="day">12/28(二)</span></div>
+                <div><span className="day">12/29(三)</span></div>
+                <div><span className="day">12/30(四)</span></div>
+                <div><span className="day">12/31(五)</span></div>
+                <div><span className="day"><i>2018</i>01/01(六)</span></div>
+                <div><span className="day">01/02(日)</span></div>
+              </div>
+              <div className="content_box">
+                <div className="row cfx">
+                  <div className="col1" onClick={(e) => this.props.whenClick(e.currentTarget)}>— —</div>
+                  <div className="col2" onClick={(e) => this.props.whenClick(e.currentTarget)}>
+                    <label>最便宜</label>
+                    <span className="price">
+                      $15,568
+                  </span>
+                    起
+              </div>
+                  <div className="col3">
+                    <span className="price">
+                      $15,568
+                 </span>
+                    起
+              </div>
+                  <div className="col4">
+                    <span className="price">
+                      $15,568
+                </span>
+                    起
+              </div>
+                  <div className="col5">
+                    <span className="price">
+                      $15,568
+                </span>
+                    起
+              </div>
+                  <div className="col6">
+                    <span className="price">
+                      $15,568
+                </span>
+                    起
+              </div>
+                  <div className="col7">
+                    <span className="price">
+                      $15,568
+                </span>
+                    起
+                </div>
+                </div>
+                <div className="row cfx">
+                  <div className="col1">查看</div>
+                  <div className="col2">
+                    <label>最便宜</label>
+                    <span className="price">
+                      $15,568
+                </span>
+                    起
+              </div>
+                  <div className="col3">
+                    <span className="price">
+                      $15,568
+                </span>
+                    起
+              </div>
+                  <div className="col4">
+                    <span className="price">
+                      $15,568
+                </span>
+                    起
+              </div>
+                  <div className="col5">
+                    <span className="price">
+                      $15,568
+                </span>
+                    起
+              </div>
+                  <div>
+                    <span className="price">
+                      $15,568
+                </span>
+                    起
+              </div>
+                  <div>
+                    <span className="price">
+                      $15,568
+                </span>
+                    起
+                </div>
+                </div>
+                <div className="row cfx">
+                  <div className="col1">查看</div>
+                  <div className="col2">
+                    <label>最便宜</label>
+                    <span className="price">
+                      $15,568
+                </span>
+                    起
+              </div>
+                  <div className="col3">
+                    <span className="price">
+                      $15,568
+                </span>
+                    起
+              </div>
+                  <div className="col4">
+                    <span className="price">
+                      $15,568
+                </span>
+                    起
+              </div>
+                  <div className="col5">
+                    <span className="price">
+                      $15,568
+                </span>
+                    起
+              </div>
+                  <div className="col6">
+                    <span className="price">
+                      $15,568
+                </span>
+                    起
+              </div>
+                  <div className="col7">
+                    <span className="price">
+                      $15,568
+                </span>
+                    起
+                </div>
+                </div>
+                <div className="row cfx">
+                  <div className="col1">— —</div>
+                  <div className="col2">
+                    <label>最便宜</label>
+                    <span className="price">
+                      $15,568
+                </span>
+                    起
+              </div>
+                  <div className="col3">
+                    <span className="price">
+                      $15,568
+                </span>
+                    起
+              </div>
+                  <div className="col4">
+                    <span className="price">
+                      $15,568
+                </span>
+                    起
+              </div>
+                  <div className="col5">
+                    <span className="price">
+                      $15,568
+                </span>
+                    起
+              </div>
+                  <div className="col6">
+                    <span className="price">
+                      $15,568
+                </span>
+                    起
+              </div>
+                  <div className="col7">
+                    <span className="price">
+                      $15,568
+                </span>
+                    起
+                </div>
+                </div>
+                <div className="row cfx">
+                  <div className="col1">— —</div>
+                  <div className="col2">
+                    <label>最便宜</label>
+                    <span className="price">
+                      $15,568
+                </span>
+                    起
+              </div>
+                  <div className="col3">
+                    <span className="price">
+                      $15,568
+                </span>
+                    起
+              </div>
+                  <div className="col4">
+                    <span className="price">
+                      $15,568
+                </span>
+                    起
+              </div>
+                  <div className="col5">
+                    <span className="price">
+                      $15,568
+                </span>
+                    起
+              </div>
+                  <div className="col6">
+                    <span className="price">
+                      $15,568
+                </span>
+                    起
+              </div>
+                  <div className="col7">
+                    <span className="price">
+                      $15,568
+                </span>
+                    起
+                </div>
+                </div>
+                <div className="row cfx">
+                  <div className="col1">— —</div>
+                  <div className="col2">
+                    <label>最便宜</label>
+                    <span className="price">
+                      $15,568
+                </span>
+                    起
+              </div>
+                  <div className="col3">
+                    <span className="price">
+                      $15,568
+                </span>
+                    起
+              </div>
+                  <div className="col4">
+                    <span className="price">
+                      $15,568
+                </span>
+                    起
+              </div>
+                  <div className="col5">
+                    <span className="price">
+                      $15,568
+                </span>
+                    起
+              </div>
+                  <div className="col6">
+                    <span className="price">
+                      $15,568
+                </span>
+                    起
+              </div>
+                  <div className="col7">
+                    <span className="price">
+                      $15,568
+                </span>
+                    起
+                </div>
+                </div>
+                <div className="row cfx">
+                  <div className="col1">— —</div>
+                  <div className="col2">
+                    <label>最便宜</label>
+                    <span className="price">
+                      $15,568
+                </span>
+                    起
+              </div>
+                  <div className="col3">
+                    <span className="price">
+                      $15,568
+                </span>
+                    起
+              </div>
+                  <div className="col4">
+                    <span className="price">
+                      $15,568
+                </span>
+                    起
+              </div>
+                  <div className="col5">
+                    <span className="price">
+                      $15,568
+                </span>
+                    起
+              </div>
+                  <div className="col6">
+                    <span className="price">
+                      $15,568
+                </span>
+                    起
+              </div>
+                  <div className="col7">
+                    <span className="price">
+                      $15,568
+                </span>
+                    起
+                </div>
+                </div>
+              </div>
+            </div>
+
+
+          </div>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
+
 }
 
-export default App;
+
+// export default App;
--- src/index.js
+++ src/index.js
@@ -4,7 +4,20 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App 
+    count= {{
+        // M版時每次點擊往前往後移動幾格儲存格
+        slide: 1, // [number] 
+        // M版時一個畫面show幾格儲存格
+        show: 3 // [number] 
+    }}
+    // 設定花多久時間移動完成
+    speed= {.3} // [number] 
+    // 每次點擊儲存格時會執行此callback，並帶入所點擊的儲存格jquery物件
+    whenClick={ (elem) => {
+        console.log(elem.className);
+    }}
+/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
