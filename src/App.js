325415752e5a2373f7d3f56f38004c38b2a71857
jiahao <a32112354jhj@gmail.com>
2019-05-22 01:30:36 +0800

    date

--- src/App.js
+++ src/App.js
@@ -11,10 +11,10 @@ export default class App extends Component {
 
   state = {
     scroll_position: 0, //捲動框位置
-    btn_l_status: 1, //左按鈕狀態
-    btn_r_status: 0, //右按鈕狀態
-    point_x:0, //點擊x
-    point_y:0, //點擊y
+    btn_l_status: 0, //左按鈕狀態
+    btn_r_status: 1, //右按鈕狀態
+    point_x: 0, //點擊x
+    point_y: 0, //點擊y
     scroll_style: {
       transition: this.props.speed + 's',
       left: 0 + '%'
@@ -52,45 +52,25 @@ export default class App extends Component {
     if (this.show == 4 && scroll_position <= -75) {
       scroll_position = -75;
       this.setState({
-        btn_l_status: 0
+        btn_r_status: 0
       });
     }
     else if (this.show == 3 && scroll_position <= -133.333) {
       scroll_position = -133.333;
       this.setState({
-        btn_l_status: 0
+        btn_r_status: 0
       });
     }
     else if (this.show == 2 && scroll_position <= -250) {
       scroll_position = -250;
       this.setState({
-        btn_l_status: 0
+        btn_r_status: 0
       });
     }
     else if (this.show == 1 && scroll_position <= -600) {
       scroll_position = -600;
       this.setState({
-        btn_l_status: 0
-      });
-    }
-
-    this.setState({
-      scroll_position: scroll_position,
-      btn_r_status: 1,
-      scroll_style: {
-        transition: this.props.speed + 's',
-        left: scroll_position + '%',
-      }
-    });
-  }
-
-  slideRight = () => {
-    let scroll_position = this.state.scroll_position + (100 / this.show) * this.slide;
-
-    if (scroll_position >= 0) {
-      scroll_position = 0;
-      this.setState({
-        btn_r_status: 0,
+        btn_r_status: 0
       });
     }
 
@@ -104,17 +84,38 @@ export default class App extends Component {
     });
   }
 
-  handleClick = (x,y, e) => {
+  slideRight = () => {
+    let scroll_position = this.state.scroll_position + (100 / this.show) * this.slide;
+
+    if (scroll_position >= 0) {
+      scroll_position = 0;
+      this.setState({
+        btn_l_status: 0,
+      });
+    }
 
     this.setState({
-      point_x:x,
-      point_y:y
+      scroll_position: scroll_position,
+      btn_r_status: 1,
+      scroll_style: {
+        transition: this.props.speed + 's',
+        left: scroll_position + '%',
+      }
+    });
+  }
+
+
+  handleClick = (x, y, e) => {
+    this.setState({
+      point_x: x,
+      point_y: y
     });
     this.props.whenClick(e.currentTarget);
   }
+  
 
   render() {
-    let DDate = Data.data;
+    let tripData = Data.data;
     return (
       <div className="App">
         <div className={"fz_table_box cfx" + " mob_" + this.show}>
@@ -125,65 +126,55 @@ export default class App extends Component {
             </div>
 
             {
-              DDate.map((item, idx) => {
-                item.data.map(() => {
-                })
-                return <div key={idx}><span className="day">{item.date_year && <i>2019</i>}{item["date"]}</span></div>
+              // 日期直
+              tripData.map((item, key) => {
+                return <div><span className="day">{item.date_year && <i>{item.date_year}</i>}{item.date}</span></div>
               })
-
             }
-
-
-            {/* <div><span className="day">12/27(一)</span></div>
-            <div><span className="day">12/28(二)</span></div>
-            <div><span className="day">12/29(三)</span></div>
-            <div><span className="day">12/30(四)</span></div>
-            <div><span className="day">12/31(五)</span></div>
-            <div><span className="day"><i>2018</i>01/01(六)</span></div>
-            <div><span className="day">01/02(日)</span></div> */}
 
           </div>
           {/* 右框 */}
           <div className="right_box">
             {/* 左按鈕 */}
-            <button type="button" className={"slide_left slide_btn " + this.btn_show[this.state.btn_l_status]} onClick={this.slideLeft}>
+            <button type="button" className={"slide_left slide_btn " + this.btn_show[this.state.btn_l_status]} onClick={this.slideRight}>
               <i className="fas fa-chevron-left"></i>
             </button>
             {/* 右按鈕 */}
-            <button type="button" className={"slide_right slide_btn " + this.btn_show[this.state.btn_r_status]} onClick={this.slideRight}>
+            <button type="button" className={"slide_right slide_btn " + this.btn_show[this.state.btn_r_status]} onClick={this.slideLeft}>
               <i className="fas fa-chevron-right"></i>
             </button>
 
             <div className="scroll_box" style={this.state.scroll_style}>
-              {/* 日期 */}
-              <div className="top_title">
-                <div><span className="day">12/27(一)</span></div>
-                <div><span className="day">12/28(二)</span></div>
-                <div><span className="day">12/29(三)</span></div>
-                <div><span className="day">12/30(四)</span></div>
-                <div><span className="day">12/31(五)</span></div>
-                <div><span className="day"><i>2019</i>01/01(六)</span></div>
-                <div><span className="day">01/02(日)</span></div>
-              </div>
+
+              {/* 日期橫 */
+                tripData.map((item, key) => {
+                  return (
+                    <div className="top_title">
+                      <div><span className="day">{item.date_year && <i>{item.date_year}</i>}{item.date}</span></div>
+                    </div>
+                  )
+
+                })
+              }
+
 
               <div className="content_box">
                 <div className="row cfx">
-                
-                  { 
-                    DDate.map((item, key)=> {
-                      return item.data.map((row, key2)=> {
-                        return (
-                          <div key={key2} className={classNames("col"+(key2+1)+" row"+(key+1),{active: key === this.state.point_x || key2 === this.state.point_y},{point: key === this.state.point_x && key2 === this.state.point_y})} onClick={(e) => this.handleClick(key,key2,e)}>
-                            {row.isTheCheapest &&<label></label>}
 
-                            <span className={isNaN(row.price) ?　'' : "price"}>{row.price}</span>
-                            </div>
+                  {
+                    tripData.map((item, key) => {
+                      return item.data.map((row, key2) => {
+                        return (
+                          <div key={key2} className={classNames("col" + (key2 + 1) + " row" + (key + 1), { active: key === this.state.point_x || key2 === this.state.point_y }, { point: key === this.state.point_x && key2 === this.state.point_y })} onClick={(e) => this.handleClick(key, key2, e)}>
+                            {row.isTheCheapest && <label></label>}
+                            <span className={isNaN(row.price) ? '' : "price"}>{row.price.toLocaleString()}</span>
+                          </div>
                         )
                       })
-                      
+
                     })
                   }
-                                
+
                 </div>
               </div>
             </div>
